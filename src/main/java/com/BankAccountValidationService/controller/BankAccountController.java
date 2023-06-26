d4130101ebc93e3af720fea1842ee06ba42f319e
Hemanthkumar432 <hemanth.4324@gmail.com>
2023-06-26 12:06:15 +0530

    Cross Orgin for react app

--- src/main/java/com/BankAccountValidationService/controller/BankAccountController.java
+++ src/main/java/com/BankAccountValidationService/controller/BankAccountController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +18,7 @@ public class BankAccountController
     @Autowired
     private  BankAccountService bankAccountService;
 
+    @CrossOrigin(origins = "https://bank-8na3.onrender.com/")
     @PostMapping(value = "/bank-accounts", produces = MediaType.TEXT_PLAIN_VALUE)
     public ResponseEntity<String> saveBankAccount(@RequestBody BankAccount bankAccount)
     {
